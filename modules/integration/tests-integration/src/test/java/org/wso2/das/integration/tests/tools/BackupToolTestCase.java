ef79b63b99cca56235a22f252db2dfc00ecdbf8f
Sachith Withana <swsachith@gmail.com>
2015-09-30 11:42:09 +0530

    adding initial backupToolTest

---
+++ modules/integration/tests-integration/src/test/java/org/wso2/das/integration/tests/tools/BackupToolTestCase.java
@@ -0,0 +1,177 @@
+/*
+* Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.das.integration.tests.tools;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.analytics.api.AnalyticsDataAPI;
+import org.wso2.carbon.analytics.api.CarbonAnalyticsAPI;
+import org.wso2.carbon.analytics.stream.persistence.stub.dto.AnalyticsTable;
+import org.wso2.carbon.analytics.stream.persistence.stub.dto.AnalyticsTableRecord;
+import org.wso2.carbon.analytics.webservice.stub.beans.StreamDefAttributeBean;
+import org.wso2.carbon.analytics.webservice.stub.beans.StreamDefinitionBean;
+import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
+import org.wso2.carbon.automation.test.utils.common.FileManager;
+import org.wso2.carbon.databridge.commons.Event;
+import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
+import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
+import org.wso2.das.integration.common.clients.AnalyticsWebServiceClient;
+import org.wso2.das.integration.common.clients.DataPublisherClient;
+import org.wso2.das.integration.common.clients.EventStreamPersistenceClient;
+import org.wso2.das.integration.common.utils.DASIntegrationTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class BackupToolTestCase extends DASIntegrationTest{
+    private static final String BACKUP_TOOL_TABLE_NAME = "backup.tool.persist.table";
+    private static final String STREAM_VERSION = "1.0.0";
+    private DataPublisherClient dataPublisherClient;
+    private AnalyticsWebServiceClient webServiceClient;
+    private EventStreamPersistenceClient persistenceClient;
+    private ServerConfigurationManager serverManager;
+    private AnalyticsDataAPI analyticsDataAPI;
+
+    @BeforeClass(alwaysRun = true, dependsOnGroups = "wso2.das")
+    protected void init() throws Exception {
+        super.init();
+        String session = getSessionCookie();
+        webServiceClient = new AnalyticsWebServiceClient(backendURL, session);
+        persistenceClient = new EventStreamPersistenceClient(backendURL, session);
+        String apiConf =
+                new File(this.getClass().getClassLoader().
+                        getResource("dasconfig" + File.separator + "api" + File.separator + "analytics-data-config.xml").toURI())
+                        .getAbsolutePath();
+        analyticsDataAPI = new CarbonAnalyticsAPI(apiConf);
+        analyticsDataAPI.deleteTable(-1234, BACKUP_TOOL_TABLE_NAME.replace('.', '_'));
+    }
+    @Test(groups = "wso2.das.backupTool", description = "Test backend availability of persistence service")
+    public void testBackendAvailability() throws Exception {
+        init();
+        Assert.assertTrue(persistenceClient.isBackendServicePresent(), "Method returns value other than true");
+    }
+
+    @Test(groups = "wso2.das.backupTool", description = "Adding new analytics table1", dependsOnMethods = "testBackendAvailability")
+    public void addAnalyticsTable() throws Exception {
+        StreamDefinitionBean streamDefinition = getEventStreamBean();
+        webServiceClient.addStreamDefinition(streamDefinition);
+        AnalyticsTable analyticsTable = getAnalyticsTable();
+        persistenceClient.addAnalyticsTable(analyticsTable);
+        //todo: take out this thread.sleep if can
+        Thread.sleep(15000);
+    }
+
+    @Test(groups = "wso2.das.backupTool", description = "Test if the table is created", dependsOnMethods = "addAnalyticsTable")
+    public void testPersistenceOfTable() throws Exception {
+        AnalyticsTable analyticsTable = persistenceClient.getAnalyticsTable(BACKUP_TOOL_TABLE_NAME, STREAM_VERSION);
+        Assert.assertEquals(analyticsTable.getPersist(), true, "Table persistence state is not correct");
+    }
+
+    @Test(groups = "wso2.das.backupTool", description = "Check event stream persistence", dependsOnMethods = "testPersistenceOfTable")
+    public void checkDataPersistence() throws Exception {
+        deployEventReceivers();
+        Thread.sleep(20000);
+        publishEvent(1, "Test Event 1");
+        long count = webServiceClient.getRecordCount(BACKUP_TOOL_TABLE_NAME.replace('.', '_'), 0, Long.MAX_VALUE);
+        if (count != -1) {
+            Assert.assertEquals(count, 1, "Record count is invalid");
+        }
+    }
+
+    @Test(groups = "wso2.das.backupTool", description = "Check event stream persistence", dependsOnMethods = "checkDataPersistence")
+    public void backupTableAndPurge() throws Exception {
+       runBackupTool();
+    }
+
+    private AnalyticsTable getAnalyticsTable() {
+        AnalyticsTable table = new AnalyticsTable();
+        table.setPersist(true);
+        table.setTableName(BACKUP_TOOL_TABLE_NAME);
+        table.setStreamVersion(STREAM_VERSION);
+        AnalyticsTableRecord[] records = new AnalyticsTableRecord[3];
+        AnalyticsTableRecord uuid = new AnalyticsTableRecord();
+        uuid.setPersist(true);
+        uuid.setPrimaryKey(true);
+        uuid.setIndexed(false);
+        uuid.setColumnName("uuid");
+        uuid.setColumnType("LONG");
+        uuid.setScoreParam(false);
+        records[0] = uuid;
+        AnalyticsTableRecord name = new AnalyticsTableRecord();
+        name.setPersist(true);
+        name.setPrimaryKey(false);
+        name.setIndexed(false);
+        name.setColumnName("name");
+        name.setColumnType("STRING");
+        name.setScoreParam(false);
+        records[1] = name;
+        AnalyticsTableRecord age = new AnalyticsTableRecord();
+        age.setPersist(true);
+        age.setPrimaryKey(false);
+        age.setIndexed(false);
+        age.setColumnName("_age");
+        age.setColumnType("INTEGER");
+        age.setScoreParam(false);
+        records[2] = age;
+        table.setAnalyticsTableRecords(records);
+        return table;
+    }
+
+    private StreamDefinitionBean getEventStreamBean() {
+        StreamDefinitionBean definitionBean = new StreamDefinitionBean();
+        definitionBean.setName(BACKUP_TOOL_TABLE_NAME);
+        definitionBean.setVersion(STREAM_VERSION);
+        StreamDefAttributeBean[] attributeBeans = new StreamDefAttributeBean[2];
+        StreamDefAttributeBean uuid = new StreamDefAttributeBean();
+        uuid.setName("uuid");
+        uuid.setType("LONG");
+        attributeBeans[0] = uuid;
+        StreamDefAttributeBean name = new StreamDefAttributeBean();
+        name.setName("name");
+        name.setType("STRING");
+        attributeBeans[1] = name;
+        definitionBean.setPayloadData(attributeBeans);
+        return definitionBean;
+    }
+
+    private void deployEventReceivers() throws IOException {
+        String streamResourceDir = FrameworkPathUtil.getSystemResourceLocation() + "tools" + File.separator;
+        String streamsLocation = FrameworkPathUtil.getCarbonHome() + File.separator + "repository"
+                + File.separator + "deployment" + File.separator + "server" + File.separator + "eventreceivers" + File.separator;
+        FileManager.copyResourceToFileSystem(streamResourceDir + "backupToolTable.xml", streamsLocation, "backupToolTable.xml");
+    }
+
+    private void publishEvent(long id, String name) throws Exception {
+        Event event = new Event(null, System.currentTimeMillis(), new Object[0], new Object[0], new Object[]{id, name});
+        dataPublisherClient = new DataPublisherClient();
+        dataPublisherClient.publish(BACKUP_TOOL_TABLE_NAME, STREAM_VERSION, event);
+        Thread.sleep(10000);
+        dataPublisherClient.shutdown();
+        analyticsDataAPI.waitForIndexing(MultitenantConstants.SUPER_TENANT_ID, BACKUP_TOOL_TABLE_NAME.replace('.', '_').toUpperCase(), 10000L);
+    }
+
+    private void runBackupTool() throws IOException {
+        String filePath = FrameworkPathUtil.getCarbonHome() + File.separator + "bin" + File.separator + "analytics-backup.sh";
+        ProcessBuilder pb = new ProcessBuilder(filePath, "-backupRecordStore", "tenantId -1234", "tables " + BACKUP_TOOL_TABLE_NAME.replace('.', '_'),
+                "dir '/home/sachith/temp/test'");
+        pb.directory(new File("/home/sachith/temp/test"));
+        Process p = pb.start();
+    }
+}
